6b31b98b98f79d83afeb04ed4e1b271995501f7c
Damian Suchy <damian.kropka@gmail.com>
2016-08-18 23:43:28 +0200

    Implemented Config

--- KitZ/KitZ.cs
+++ KitZ/KitZ.cs
@@ -1,28 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
 using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
 
 namespace KitZ
 {
+    [ApiVersion(1, 23)]
     public class KitZ : TerrariaPlugin
     {
+        public static Config Config { get; private set; }
+
         public KitZ(Main game) : base(game)
         {
         }
 
         public override void Initialize()
         {
-            throw new NotImplementedException();
+            GeneralHooks.ReloadEvent += OnReload;
+            PlayerHooks.PlayerCommand += OnPlayerCommand;
+
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
         }
 
-        public override string Author { get { return "Renerte"; } }
-        public override string Description { get { return "Customizable kits for your TShock server!"; } }
-        public override string Name { get { return "KitZ"; } }
-        public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+        public override string Author => "Renerte";
+        public override string Description => "Customizable kits for your TShock server!";
+        public override string Name => "KitZ";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        private void OnPlayerCommand(PlayerCommandEventArgs e)
+        {
+            if (e.Handled || e.Player == null)
+            {
+                return;
+            }
+
+            Command command = e.CommandList.FirstOrDefault();
+            if (command == null || (command.Permissions.Any() && !command.Permissions.Any(s => e.Player.Group.HasPermission(s))))
+            {
+                return;
+            }
+        }
+
+        private void OnReload(ReloadEventArgs e)
+        {
+            string path = Path.Combine(TShock.SavePath, "kitz.json");
+            Config = Config.Read(path);
+            if (!File.Exists(path))
+                Config.Write(path);
+            e.Player.SendSuccessMessage($"[KitZ] {Config.ReloadSuccess}");
+        }
+
+        private void OnInitialize(EventArgs e)
+        {
+            string path = Path.Combine(TShock.SavePath, "kitz.json");
+            Config = Config.Read(path);
+            if (!File.Exists(path))
+                Config.Write(path);
+        }
     }
 }
