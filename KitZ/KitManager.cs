e21063bd24b0bf696b2dd0822880c26933c20f0c
Damian Suchy <damian.kropka@gmail.com>
2016-08-19 01:10:47 +0200

    Added KitManager and 'Kits' SQL table

--- KitZ/KitManager.cs
+++ KitZ/KitManager.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using TShockAPI.DB;
 
 namespace KitZ
 {
-    class KitManager
+    public class KitManager
     {
+        private IDbConnection db;
+        private ReaderWriterLockSlim slimLock = new ReaderWriterLockSlim();
+
+        public KitManager(IDbConnection db)
+        {
+            this.db = db;
+
+            var sqlCreator = new SqlTableCreator(db,
+                db.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder) new SqliteQueryCreator() : new MysqlQueryCreator());
+
+            sqlCreator.EnsureTableStructure(new SqlTable("Kits",
+                new SqlColumn("ID", MySqlDbType.Int32) { AutoIncrement = true, Primary = true},
+                new SqlColumn("Name", MySqlDbType.Text) {Unique = true},
+                new SqlColumn("Items", MySqlDbType.Text),
+                new SqlColumn("Quantities", MySqlDbType.Text),
+                new SqlColumn("MaxUses", MySqlDbType.Int32),
+                new SqlColumn("RefreshTime", MySqlDbType.Int32)));
+
+            //TODO: KitUses table
+        }
     }
 }
