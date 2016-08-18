30c2ae826e9d9cc1d53aa900bb0a5133906c2537
Damian Suchy <damian.kropka@gmail.com>
2016-08-18 21:41:47 +0200

    Main files

---
+++ KitZ/KitZ.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace KitZ
+{
+    public class KitZ : TerrariaPlugin
+    {
+        public KitZ(Main game) : base(game)
+        {
+        }
+
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Author { get { return "Renerte"; } }
+        public override string Description { get { return "Customizable kits for your TShock server!"; } }
+        public override string Name { get { return "KitZ"; } }
+        public override Version Version { get { return Assembly.GetExecutingAssembly().GetName().Version; } }
+    }
+}
