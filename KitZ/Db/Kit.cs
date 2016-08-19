c4beab23cbf49b8862bfb650b81af6f575452277
Damian Suchy <damian.kropka@gmail.com>
2016-08-19 15:02:53 +0200

    Added Kit class and 'KitUses' table

---
+++ KitZ/Db/Kit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace KitZ.Db
+{
+    public class Kit
+    {
+        public Kit(string name, List<int> itemList, int maxUses, int refreshTime)
+        {
+            Name = name;
+            ItemList = itemList;
+            MaxUses = maxUses;
+            RefreshTime = refreshTime;
+        }
+
+        public string Name { get; private set; }
+        public List<int> ItemList { get; private set; }
+        public int MaxUses { get; private set; }
+        public int RefreshTime { get; private set; }
+    }
+}
--- KitZ/Db/KitManager.cs
+++ KitZ/Db/KitManager.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using TShockAPI.DB;
 
-namespace KitZ
+namespace KitZ.Db
 {
     public class KitManager
     {
         private IDbConnection db;
+        private List<Kit> Kits = new List<Kit>();
         private ReaderWriterLockSlim slimLock = new ReaderWriterLockSlim();
 
         public KitManager(IDbConnection db)
@@ -23,14 +20,18 @@ namespace KitZ
                 db.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder) new SqliteQueryCreator() : new MysqlQueryCreator());
 
             sqlCreator.EnsureTableStructure(new SqlTable("Kits",
-                new SqlColumn("ID", MySqlDbType.Int32) { AutoIncrement = true, Primary = true},
+                new SqlColumn("ID", MySqlDbType.Int32) {AutoIncrement = true, Primary = true},
                 new SqlColumn("Name", MySqlDbType.Text) {Unique = true},
                 new SqlColumn("Items", MySqlDbType.Text),
                 new SqlColumn("Quantities", MySqlDbType.Text),
                 new SqlColumn("MaxUses", MySqlDbType.Int32),
                 new SqlColumn("RefreshTime", MySqlDbType.Int32)));
 
-            //TODO: KitUses table
+            sqlCreator.EnsureTableStructure(new SqlTable("KitUses",
+                new SqlColumn("ID", MySqlDbType.Int32) {AutoIncrement = true, Primary = true},
+                new SqlColumn("UserID", MySqlDbType.Int32),
+                new SqlColumn("KitID", MySqlDbType.Int32),
+                new SqlColumn("ExpireTime", MySqlDbType.DateTime)));
         }
     }
 }
