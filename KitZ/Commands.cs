f86c1eb12cc11d873689218c2bf8de8beaf83cb3
Damian Suchy <damian.kropka@gmail.com>
2016-08-22 00:47:36 +0200

    Fix /kit command

--- KitZ/Commands.cs
+++ KitZ/Commands.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Linq;
 using System.Reflection;
-using KitZ.Db;
 using TShockAPI;
 
 namespace KitZ
@@ -22,25 +19,23 @@ namespace KitZ
                 e.Player.SendErrorMessage(KitZ.Config.NoKitEntered);
                 return;
             }
-            if (!e.Player.HasPermission($"kitz.use.{e.Parameters.First()}"))
+            if (!e.Player.HasPermission($"kitz.use.{e.Parameters[0]}"))
             {
-                e.Player.SendErrorMessage(string.Format(KitZ.Config.KitNoPerm), e.Parameters.First());
+                e.Player.SendErrorMessage(string.Format(KitZ.Config.KitNoPerm, e.Parameters[0]));
                 return;
             }
-            e.Player.SendInfoMessage(string.Format(KitZ.Config.KitGiven, e.Parameters.First()));
 
             var kit = await KitZ.Kits.GetAsync(e.Parameters.First());
             if (kit != null)
             {
+                e.Player.SendInfoMessage(string.Format(KitZ.Config.KitGiven, e.Parameters[0]));
                 foreach (var kitItem in kit.ItemList)
-                {
                     e.Player.SendInfoMessage(
-                        $"{TShock.Utils.GetPrefixById(kitItem.Modifier)} {TShock.Utils.GetItemById(kitItem.Id)}x{kitItem.Amount}");
-                }
+                        $"{TShock.Utils.GetPrefixById(kitItem.Modifier)} {TShock.Utils.GetItemById(kitItem.Id).name} x {kitItem.Amount}");
             }
             else
             {
-                e.Player.SendErrorMessage(string.Format(KitZ.Config.KitNotFound), e.Parameters.First());
+                e.Player.SendErrorMessage(string.Format(KitZ.Config.KitNotFound, e.Parameters[0]));
             }
         }
     }
