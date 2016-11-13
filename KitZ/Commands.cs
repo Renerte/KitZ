d6c7fdd66f9ca78d1a9fa876b18da505d9a8bd2e
Damian Suchy <damian.kropka@gmail.com>
2016-11-13 21:00:15 +0100

    Fixed NullReferenceException related to kit uses for kit's without them

--- KitZ/Commands.cs
+++ KitZ/Commands.cs
@@ -58,6 +58,7 @@ namespace KitZ
                             TShock.Utils.GetItemById(kitItem.Id).name));
                 }
                 var kitUse = await KitZ.Kits.GetKitUseAsync(e.Player, kit);
+                if (kitUse == null) return;
                 await Task.Delay(kitUse.ExpireTime - DateTime.UtcNow);
                 await KitZ.Kits.DeleteKitUseAsync(kitUse);
             }
